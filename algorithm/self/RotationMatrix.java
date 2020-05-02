6636396a6b06dced03a5ebbb08c74cafb298ce0c
yangpengwei <yangpengwei@xdf.cn>
2020-05-02 10:27:19 +0800

    增加题目

---
+++ algorithm/self/RotationMatrix.java
@@ -0,0 +1,73 @@
+package algorithm.self;
+
+import java.util.Arrays;
+
+/**
+ * 旋转矩阵
+ * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+ *
+ * 不占用额外内存空间能否做到
+ *
+ * 给定 matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ * 示例2：
+ * 给定 matrix =
+ * [
+ *   [ 5, 1, 9,11],
+ *   [ 2, 4, 8,10],
+ *   [13, 3, 6, 7],
+ *   [15,14,12,16]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [15,13, 2, 5],
+ *   [14, 3, 4, 1],
+ *   [12, 6, 8, 9],
+ *   [16, 7,10,11]
+ * ]
+ */
+public class RotationMatrix {
+
+    public static void main(String[] args) {
+        int[][] arr = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        int[][] rotate = rotate(arr);
+        for (int[] ints : rotate) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
+
+    public static int[][] rotate(int[][] matrix) {
+
+        int length = matrix.length;
+        // 以对角线为轴进行旋转
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i+1; j < length ; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //  中间点替换
+        for (int i = 0; i < length ; i++) {
+            for (int j = 0; j < length/2 ; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][length - 1 - j];
+                matrix[i][length - 1 - j] = tmp;
+            }
+        }
+        return matrix;
+    }
+
+}
