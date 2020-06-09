d794002c8e47908d584846f5bad910142aebcfe0
yangpengwei <yangpengwei@xdf.cn>
2020-06-09 13:57:47 +0800

    增加题目

--- leetcode/solution680.java
+++ leetcode/solution680.java
@@ -2,18 +2,18 @@ package leetcode;
 
 /**
  * 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: "aba"
  * 输出: True
  * 示例 2:
- *
+ * <p>
  * 输入: "abca"
  * 输出: True
  * 解释: 你可以删除c字符。
  * 注意:
- *
+ * <p>
  * 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000
  */
 public class solution680 {
@@ -26,22 +26,55 @@ public class solution680 {
 
     public static boolean validPalindrome(String s) {
 
-        for (int i = 0 , j = s.length()-1; i < j ; i++,j--) {
-            if(s.charAt(i) != s.charAt(j)){
-                return isPalindrome(s,i,j-1) || isPalindrome(s,i+1,j) ;
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
             }
         }
-        return  true;
+        return true;
     }
 
-    public static boolean isPalindrome(String s,int i ,int j){
+    public static boolean isPalindrome(String s, int i, int j) {
 
-        while (i<j){
-            if(s.charAt(i++) != s.charAt(j--)){
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
                 return false;
             }
         }
+        return true;
+    }
 
+    /**
+     * 解法2 双指针移动，匹配字符串
+     * @param s
+     * @return
+     */
+    public boolean validPalindrome2(String s) {
+        int low = 0, high = s.length() - 1;
+        while (low < high) {
+            char c1 = s.charAt(low), c2 = s.charAt(high);
+            if (c1 == c2) {
+                low++;
+                high--;
+            } else {
+                boolean flag1 = true, flag2 = true;
+                for (int i = low, j = high - 1; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
+                        flag1 = false;
+                        break;
+                    }
+                }
+                for (int i = low + 1, j = high; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
+                        flag2 = false;
+                        break;
+                    }
+                }
+                return flag1 || flag2;
+            }
+        }
         return true;
     }
 }
