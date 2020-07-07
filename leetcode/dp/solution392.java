f777aa29aad3b8b5d49ea4f02a8e33b73f5a992f
yangpengwei <yangpengwei@xdf.cn>
2020-07-07 11:16:39 +0800

    增加动态规划解法

--- leetcode/dp/solution392.java
+++ leetcode/dp/solution392.java
@@ -30,8 +30,9 @@ public class solution392 {
 
     public static void main(String[] args) {
 
-//        System.out.println(isSubsequence("abc","ahbgdc"));
-        System.out.println(isSubsequence("acb","ahbgdc"));
+        System.out.println(isSubsequence2("abc","ahbgdc"));
+//        System.out.println(isSubsequence2("acb","ahbgdc"));
+//        System.out.println(isSubsequence2("axc","ahbgdc"));
 
     }
 
@@ -59,13 +60,43 @@ public class solution392 {
     /**
      * 动态规划解法
      *
+     * 状态：dp[i][j]为s的从头开始到i的子字符串是否为t从头开始到j的子字符串的子序列
+     *
+     * 状态转移公式：
+     *
+     * 当char[i]==char[j]时，则字符i一定是j的子序列，如果0~i-1子字符串是0~j-1子字符串的子序列，则dp[i][j]=true，所以dp[i][j] = dp[i-1][j-1]；
+     * 当char[i]!=char[i]时，即判断当前0~i子字符串是否是0~j-1的子字符串的子序列，即dp[i][j] = dp[i][j - 1]
+     * 如ab，eabc，虽然s的最后一个字符和t中最后一个字符不相等，但是因为ab是eab的子序列，所以ab也是eabc的子序列
+     * 初始化：空字符串一定是t的子字符串的子序列，所以dp[0][j]=true
+     *
+     *
      * @param s
      * @param t
      * @return
      */
     public static boolean isSubsequence2(String s, String t) {
 
-        return false;
+        int sn = s.length();
+        int tn = t.length();
+        // 定义备忘录
+        boolean[][] dp = new boolean[sn+1][tn+1];
+        // 初始化
+        dp[0][0] = true;
+        for (int i = 0; i < dp[0].length ; i++) {
+            dp[0][i] = true;
+        }
+        // 推导结果
+        for (int i = 1; i <= sn ; i++) {
+            for (int j = 1; j <= tn ; j++) {
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j]=dp[i][j-1];
+
+                }
+            }
+        }
+        return dp[sn][tn];
     }
 
 }
