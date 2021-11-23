94840dd73831e05ae3d5624155dbad5d449dcc9f
nanluojuexing <nanluojuexing@gmail.com>
2021-11-23 23:39:17 +0800

    增加数组去排列的问题

--- leetcode/recall/solution46.java
+++ leetcode/recall/solution46.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -34,28 +35,80 @@ public class solution46 {
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        // 标记位置
         int[] visited = new int[nums.length];
         recall(res,nums,new ArrayList<>(),visited);
         return res;
     }
 
+    /**
+     * 递归存储元素
+     * @param res 存储所有最终的结果集
+     * @param nums 数组
+     * @param temp 存储一组排列
+     * @param visited 标记是否处理过
+     */
     private void recall(List<List<Integer>> res, int[] nums, ArrayList<Integer> temp, int[] visited) {
+        // 终止条件，当数组长度等于临时数组长度，说明所有元素已遍历
         if(nums.length == temp.size()){
-            res.add(new ArrayList<Integer>(temp));
+            res.add(new ArrayList<>(temp));
             return ;
         }
-
+        // 遍历数组
         for (int i = 0; i < nums.length; i++) {
+            // 如果元素已存储，则跳过
             if(visited[i] ==1){
                 continue;
             }
+            // 标记该未知的元素的为已使用
             visited[i] = 1;
+            // 元素加入数组中
             temp.add(nums[i]);
+            // 递归遍历
             recall(res,nums,temp,visited);
+            // 处理完后，标记为改为0
             visited[i] = 0;
             temp.remove(temp.size()-1);
         }
-
     }
+
+    /**
+     * 解法2
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>>  permute2(int[] nums){
+        if(nums == null || nums.length == 0){
+            return new ArrayList<>();
+        }
+        // 存储最终的结果
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : nums) {
+            list.add(num);
+        }
+        permuteRec(list,0,res);
+        return res;
+    }
+
+    /**
+     *
+     * @param nums
+     * @param start
+     * @param res
+     */
+    private void permuteRec(List<Integer> nums, int start, List<List<Integer>> res) {
+        if(start == nums.size()){
+            res.add(new ArrayList<>(nums));
+        }else {
+            for (int j = start; j < nums.size(); j++) {
+                Collections.swap(nums,j,start);
+                permuteRec(nums,j+1,res);
+                Collections.swap(nums,j,start);
+            }
+        }
+    }
+
 
 }
--- leetcode/recall/solution47.java
+++ leetcode/recall/solution47.java
@@ -1,8 +1,118 @@
 package leetcode.recall;
 
+import org.junit.Test;
+
+import java.util.*;
+
 /**
  * 回溯算法
+ *
+ * 全排列
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ *  [1,2,1],
+ *  [2,1,1]]
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * 链接：https://leetcode-cn.com/problems/permutations-ii
+ *
  */
 public class solution47 {
+
+
+    @Test
+    public void test(){
+        int[] nums={1,1,2};
+        int[] nums2 = {1,2,3};
+        int[] nums3 = {3,3,3,0};
+        System.out.println(Arrays.toString(permuteUnique2(nums).toArray()));
+        System.out.println(Arrays.toString(permuteUnique2(nums2).toArray()));
+        System.out.println(Arrays.toString(permuteUnique2(nums3).toArray()));
+    }
+
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums.length ==0){
+            return result;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        permuteUniqueHelper(result,list,nums,0,visited);
+        return result;
+    }
+
+    /**
+     *
+     * @param result
+     * @param list
+     * @param start
+     */
+    private void permuteUniqueHelper(List<List<Integer>> result, List<Integer> list, int[] nums,int start,boolean[] visited) {
+        if(start == nums.length){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if(visited[i] || (i>0 && nums[i] == nums[i-1 ] && !visited[i-1])){
+                continue;
+            }
+            list.add(nums[i]);
+            visited[i] = true;
+            permuteUniqueHelper(result,list,nums,start+1,visited);
+            visited[i] = false;
+            list.remove(start);
+        }
+    }
+
+
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums.length ==0){
+            return result;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        permuteUniqueHelper2(result,list,0);
+        return result;
+    }
+
+    /**
+     *
+     * @param result
+     * @param list
+     * @param start
+     */
+    private void permuteUniqueHelper2(List<List<Integer>> result, List<Integer> list,int start) {
+        if(start == list.size()){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        Set<Integer> set = new HashSet();
+        for (int i = start; i < list.size(); i++) {
+
+            if(set.add(list.get(i))){
+                Collections.swap(list,i,start);
+                permuteUniqueHelper2(result,list,start+1);
+                Collections.swap(list,i,start);
+            }
+        }
+    }
+
 
 }
